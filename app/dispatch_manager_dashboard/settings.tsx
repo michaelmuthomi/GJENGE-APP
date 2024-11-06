3331f3c43876e25250c54f5910445d98b3bc4e1e
michaelmuthomi <lit911call@gmail.com>
2024-11-06 11:33:53 +0300

    feat: implement setting pages to the rest of the users

---
+++ app/dispatch_manager_dashboard/settings.tsx
@@ -0,0 +1,116 @@
+import { Link } from "expo-router";
+import React, { useCallback, useRef } from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { H2, H3, H4, P } from "~/components/ui/typography";
+import { Separator } from "~/components/ui/separator";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import BottomSheet, {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Feedback } from "~/components/sheets/feedback";
+import { AboutUs } from "~/components/sheets/aboutus";
+import { ContactUs } from "~/components/sheets/contactus";
+import { Account } from "~/components/sheets/account";
+import { Orders } from "~/components/sheets/orders";
+
+const SettingsPage = () => {
+  return (
+    <View className=" bg-[#060606] flex-1 pt-14 px-6 gap-10">
+      <View className="flex justify-center">
+        <H4
+          className="capitalize color-white"
+          style={{ fontFamily: "Inter_600SemiBold" }}
+        >
+          Settings
+        </H4>
+      </View>
+      <ScrollView>
+        <View className="flex-1">
+          <Account
+            sheetTrigger={
+              <H4
+                className="capitalize color-white"
+                style={{ fontFamily: "Inter_400Regular" }}
+              >
+                Account info &rarr;
+              </H4>
+            }
+          />
+          <Separator className="my-6 bg-muted-foreground" />
+          <Orders
+            sheetTrigger={
+              <H4
+                className="capitalize color-white"
+                style={{ fontFamily: "Inter_400Regular" }}
+              >
+                Orders &rarr;
+              </H4>
+            }
+          />
+          <Separator className="my-6 bg-muted-foreground" />
+          <ContactUs
+            sheetTrigger={
+              <H4
+                className="capitalize color-white"
+                style={{ fontFamily: "Inter_400Regular" }}
+              >
+                Contact Us &rarr;
+              </H4>
+            }
+          />
+          <Separator className="my-6 bg-muted-foreground" />
+          <AboutUs
+            sheetTrigger={
+              <H4
+                className="color-white"
+                style={{ fontFamily: "Inter_500Medium" }}
+              >
+                About us &rarr;
+              </H4>
+            }
+          />
+          <Separator className="my-6 bg-muted-foreground" />
+          <Feedback
+            sheetTrigger={
+              <H4
+                className="color-white"
+                style={{ fontFamily: "Inter_500Medium" }}
+              >
+                Send us Feedback &rarr;
+              </H4>
+            }
+          />
+          <Separator className="my-6 bg-muted-foreground" />
+          <Link
+            href={{ pathname: "/screens/LoginScreen" }}
+            className="w-full color-white"
+          >
+            <H4
+              className="color-white"
+              style={{ fontFamily: "Inter_500Medium" }}
+            >
+              Logout &rarr;
+            </H4>
+          </Link>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "grey",
+  },
+  contentContainer: {
+    padding: 36,
+    alignItems: "center",
+  },
+});
+
+export default SettingsPage;
---
+++ app/finance_manager_dashboard/_layout.tsx
@@ -0,0 +1,78 @@
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Tabs } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import { StyleSheet } from 'react-native';
+import Icon from "react-native-vector-icons/Ionicons";
+
+export default function TabLayout() {
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <BottomSheetModalProvider>
+        <Tabs
+          screenOptions={{
+            tabBarActiveTintColor: "#66d46f",
+            tabBarActiveBackgroundColor: "#000000fe",
+            tabBarInactiveBackgroundColor: "#000000fe",
+            tabBarStyle: { borderWidth: 0, borderColor: "#000000" },
+          }}
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: "Home",
+              tabBarShowLabel: false,
+              tabBarIcon: ({ color, size, focused }) =>
+                focused ? (
+                  <MaterialCommunityIcons
+                    name="home-variant"
+                    color={color}
+                    size={28}
+                  />
+                ) : (
+                  <MaterialCommunityIcons
+                    name="home-variant-outline"
+                    color={color}
+                    size={28}
+                  />
+                ),
+              headerShown: false,
+              tabBarLabelStyle: {
+                fontFamily: "Inter_400Regular",
+                fontSize: 10,
+              },
+            }}
+          />
+          <Tabs.Screen
+            name="settings"
+            options={{
+              title: "Settings",
+              tabBarShowLabel: false,
+              tabBarIcon: ({ color, size, focused }) =>
+                focused ? (
+                  <Icon name="cog" color={color} size={28} />
+                ) : (
+                  <Icon name="cog-outline" color={color} size={28} />
+                ),
+              headerShown: false,
+              tabBarLabelStyle: {
+                fontFamily: "Inter_400Regular",
+                fontSize: 10,
+              },
+            }}
+          />
+        </Tabs>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 36,
+    alignItems: "center",
+  },
+});
