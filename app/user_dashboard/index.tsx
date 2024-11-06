e3021f25ca8a790567c404d74b78e812b44ce9f0
michaelmuthomi <lit911call@gmail.com>
2024-11-06 19:08:08 +0300

    feat: display products in the users dashboard
     - Add product images

--- app/user_dashboard/index.tsx
+++ app/user_dashboard/index.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
-import { Image, View } from 'react-native';
-import { Button } from '~/components/ui/button';
-import { Text } from '~/components/ui/text';
-import { H1, H2, H3, H4, P } from '~/components/ui/typography';
-import GjengeLogo from "~/assets/images/gjenge-logo.png"
-import CEO from "~/assets/images/ceo.png"
-import { Link } from 'expo-router';
-import { Avatar, AvatarFallback } from '~/components/ui/avatar';
-import { useLocalSearchParams } from 'expo-router';
-import { checkUser } from '~/lib/supabase';
-import { greeting } from '~/services/greeting';
+import { Image, ScrollView, View } from "react-native";
+import { Button } from "~/components/ui/button";
+import { Text } from "~/components/ui/text";
+import { H1, H2, H3, H4, P } from "~/components/ui/typography";
+import GjengeLogo from "~/assets/images/gjenge-logo.png";
+import CEO from "~/assets/images/ceo.png";
+import { Link } from "expo-router";
+import { Avatar, AvatarFallback } from "~/components/ui/avatar";
+import { useLocalSearchParams } from "expo-router";
+import { checkUser } from "~/lib/supabase";
+import { greeting } from "~/services/greeting";
+import { Products } from "~/data/productsData";
+import variationsIMG from "~/assets/images/variations.png";
 
 interface UserDetails {
   first_name: string;
@@ -18,25 +20,110 @@ interface UserDetails {
 
 export default function UserDashboard() {
   const { email } = useLocalSearchParams();
-  const [userDetails, setUserDetails] = React.useState<UserDetails | null>(null);
-  React.useEffect(()=>{
-    async function fetchUserDetails(){
-      return await checkUser(email.toString())
+  const [userDetails, setUserDetails] = React.useState<UserDetails | null>(
+    null
+  );
+  React.useEffect(() => {
+    async function fetchUserDetails() {
+      return await checkUser(email.toString());
     }
-    fetchUserDetails().then(setUserDetails)
-    console.log(userDetails)
-  }, [])
-  
-  const user_abreviation = userDetails ? `${userDetails.first_name[0]}${userDetails.last_name[0]}` : '';
+    fetchUserDetails().then(setUserDetails);
+    console.log(userDetails);
+  }, []);
+
+  const user_abreviation = userDetails
+    ? `${userDetails.first_name[0]}${userDetails.last_name[0]}`
+    : "";
   return (
-    <View className=' bg-[#060606] flex-1 pt-14 px-6'>
-      <View className='flex justify-center'>
-        <H4 className='capitalize color-white' style={{fontFamily: "Inter_600SemiBold"}}>{greeting()}, {userDetails && userDetails['first_name']}</H4>
-        <Avatar alt="User" className='absolute right-0'>
+    <View className=" bg-[#060606] flex-1 pt-14 px-6 gap-12">
+      <View className="flex justify-center">
+        <H4
+          className="capitalize color-white"
+          style={{ fontFamily: "Inter_600SemiBold" }}
+        >
+          {greeting()}, {userDetails && userDetails["first_name"]}
+        </H4>
+        <Avatar alt="User" className="absolute right-0">
           <AvatarFallback>
-            <P className='uppercase'>{user_abreviation}</P>
+            <P className="uppercase">{user_abreviation}</P>
           </AvatarFallback>
         </Avatar>
+      </View>
+      <ScrollView>
+        <View className='gap-10'>
+          <View className="gap-4">
+            <H4
+              className="color-white text-base uppercase"
+              style={{ fontFamily: "Inter_300Light" }}
+            >
+              Product catalogue
+            </H4>
+            <ScrollView horizontal directionalLockEnabled>
+              <View className="gap-4 flex-row">
+                {Products.map((product) => (
+                  <ProductShowcase
+                    imageSrc={product["imageSrc"]}
+                    title={product["title"]}
+                    details={product["details"]}
+                    price={product["price"]}
+                    bgcolor={product["bgcolor"]}
+                  />
+                ))}
+              </View>
+            </ScrollView>
+          </View>
+          <View className="gap-4">
+            <H4
+              className="color-white text-base uppercase"
+              style={{ fontFamily: "Inter_300Light" }}
+            >
+              Available special variations
+            </H4>
+            <Image
+        source={variationsIMG}
+        style={{
+          width: "100%",
+          height: 100,
+          objectFit: "contain",
+          borderRadius: 10,
+        }}
+      />
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+function ProductShowcase(props: {
+  imageSrc: any;
+  title: string;
+  details: string;
+  price: string;
+  bgcolor: string;
+}) {
+  return (
+    <View
+      className="gap-4 overflow-auto"
+      style={{
+        width: 280,
+      }}
+    >
+      <Image
+        source={props.imageSrc}
+        style={{
+          width: "100%",
+          height: 400,
+          objectFit: "contain",
+          backgroundColor: props.bgcolor,
+          borderRadius: 10,
+        }}
+      />
+      <View>
+        <H4 className="color-white" style={{ fontFamily: "Inter_700Bold" }}>
+          {props.title}
+        </H4>
+        <P className="color-white">{props.details}</P>
       </View>
     </View>
   );
---
+++ data/productsData.ts
@@ -0,0 +1,30 @@
+import LightDutyPaverIMG from "~/assets/images/LightDutyPaver.png";
+import MediumDutyPaverIMG from "~/assets/images/medium_duty_paver.png";
+import HeavyDutyPaverIMG from "~/assets/images/HeavyDutyPaver.png";
+
+export const Products = [
+  {
+    imageSrc: LightDutyPaverIMG,
+    title: "LIGHT DUTY",
+    details:
+      "Best for Household Compounds Light & Strong, carries loads up to 100 N/mm2 or 82 metric tonnes",
+    price: "Ksh 1208",
+    bgcolor: "#111111",
+  },
+  {
+    imageSrc: MediumDutyPaverIMG,
+    title: "Medium DUTY",
+    details:
+      "Best for Commercial Premises Carries loads up to 140 N/mm2 or 115 metric tonnes",
+    price: "Ksh 1398",
+    bgcolor: "#111111",
+  },
+  {
+    imageSrc: HeavyDutyPaverIMG,
+    title: "Heavy DUTY",
+    details:
+      "Best for Roads, operation areas of Heavy Machinery Carries loads up to 200 N/mm2 or 164 metric tonnes",
+    price: "Ksh 1780",
+    bgcolor: "#111111",
+  },
+];
