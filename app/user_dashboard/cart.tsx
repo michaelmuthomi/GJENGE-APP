64922e4685cb5da7f3efb8e3769cb32ca1f18e70
michaelmuthomi <lit911call@gmail.com>
2024-11-06 11:41:02 +0300

    feat: update splash screen icon
    - Update the cart screen to match the apps layout requirements

--- app/user_dashboard/cart.tsx
+++ app/user_dashboard/cart.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Link } from "expo-router";
+import React, { useCallback, useRef } from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { H2, H3, H4, P } from "~/components/ui/typography";
+import { Separator } from "~/components/ui/separator";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import BottomSheet, {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Feedback } from "~/components/sheets/feedback";
+import { AboutUs } from "~/components/sheets/aboutus";
+import { ContactUs } from "~/components/sheets/contactus";
+import { Account } from "~/components/sheets/account";
+import { Orders } from "~/components/sheets/orders";
 
-const CartPage = () => {
+export function CartPage () {
   return (
-    <View style={styles.container}>
-      <Text>Cart Page</Text>
+    <View className=" bg-[#060606] flex-1 pt-14 px-6 gap-10">
+      <View className="flex justify-center">
+        <H4
+          className="capitalize color-white"
+          style={{ fontFamily: "Inter_600SemiBold" }}
+        >
+          Settings
+        </H4>
+      </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
-
-export default CartPage;
 
